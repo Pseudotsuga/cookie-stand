6e46f4d838bb56f61b68ebc085c00e5701f6c90f
Conor <conormccue@gmail.com>
2019-09-30 13:23:27 -0700

    shop object literals and properties

--- js/sales.js
+++ js/sales.js
@@ -1,1 +1,27 @@
 'use strict';
+
+var seattle = {
+  minimumCustomersPerHour: 23,
+  maximumCustomersPerHour: 65,
+  avgCookieSalesPerCustomer: 6.3,
+};
+var tokyo = {
+  minimumCustomersPerHour: 3,
+  maximumCustomersPerHour: 24,
+  avgCookieSalesPerCustomer: 1.2,
+};
+var dubai = {
+  minimumCustomersPerHour: 11,
+  maximumCustomersPerHour: 38,
+  avgCookieSalesPerCustomer: 3.7,
+};
+var paris = {
+  minimumCustomersPerHour: 20,
+  maximumCustomersPerHour: 38,
+  avgCookieSalesPerCustomer: 2.3,
+};
+var lima = {
+  minimumCustomersPerHour: 2,
+  maximumCustomersPerHour: 16,
+  avgCookieSalesPerCustomer: 4.6,
+};
