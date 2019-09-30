52c963d2e48dc15296d539743c38012e3ed2fa27
Conor <conormccue@gmail.com>
2019-09-30 14:02:43 -0700

    steps 3 and 4 in one: random sales calculated for each hour and pushed to array

--- js/sales.js
+++ js/sales.js
@@ -4,15 +4,26 @@ var seattle = {
   minimumCustomersPerHour: 23,
   maximumCustomersPerHour: 65,
   avgCookieSalesPerCustomer: 6.3,
+  hoursOpen: 14,
+  hourlySalesArray: [],
   randomNumberOfCustomers: function(){
     return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
   },
+  generateRandomSalesData: function (){
+    for(var i = 0; i < this.hoursOpen; i++){
+      this.hourlySalesArray.push(Math.floor(this.randomNumberOfCustomers() * this.avgCookieSalesPerCustomer));
+    }
+  },
 };
+seattle.generateRandomSalesData();
+console.log(seattle.hourlySalesArray);
 
 var tokyo = {
   minimumCustomersPerHour: 3,
   maximumCustomersPerHour: 24,
   avgCookieSalesPerCustomer: 1.2,
+  hoursOpen: 14,
+  hourlySalesArray: [],
   randomNumberOfCustomers: function(){
     return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
   },
@@ -21,6 +32,8 @@ var dubai = {
   minimumCustomersPerHour: 11,
   maximumCustomersPerHour: 38,
   avgCookieSalesPerCustomer: 3.7,
+  hoursOpen: 14,
+  hourlySalesArray: [],
   randomNumberOfCustomers: function(){
     return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
   },
@@ -29,6 +42,8 @@ var paris = {
   minimumCustomersPerHour: 20,
   maximumCustomersPerHour: 38,
   avgCookieSalesPerCustomer: 2.3,
+  hoursOpen: 14,
+  hourlySalesArray: [],
   randomNumberOfCustomers: function(){
     return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
   },
@@ -37,6 +52,8 @@ var lima = {
   minimumCustomersPerHour: 2,
   maximumCustomersPerHour: 16,
   avgCookieSalesPerCustomer: 4.6,
+  hoursOpen: 14,
+  hourlySalesArray: [],
   randomNumberOfCustomers: function(){
     return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
   },
