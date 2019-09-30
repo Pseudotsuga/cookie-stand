6d797b02b616a22eeb701eb4f8675aec5c108246
Conor <conormccue@gmail.com>
2019-09-30 15:05:11 -0700

    li tags populate with data, need to modify so appropriate hour tags are shown

--- js/sales.js
+++ js/sales.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var seattle = {
-  storename: 'seattle',
+  storename: 'Seattle',
   minimumCustomersPerHour: 23,
   maximumCustomersPerHour: 65,
   avgCookieSalesPerCustomer: 6.3,
@@ -23,17 +23,18 @@ var seattle = {
     for (var i = 0; i < this.hourlySalesArray.length; i++){
       var liElement = document.createElement('li');
       liElement.setAttribute('id', `li${i}`);
+      liElement.textContent = `${this.hourlySalesArray[i]}`;
       ulElement.appendChild(liElement);
-      console.log(liElement);
     }
   },
 };
 
+console.log(seattle.hourlySalesArray);
 seattle.generateRandomSalesData();
 seattle.renderSalesArrayAsUnorderedList();
 
 var tokyo = {
-  storename: 'tokyo',
+  storename: 'Tokyo',
   minimumCustomersPerHour: 3,
   maximumCustomersPerHour: 24,
   avgCookieSalesPerCustomer: 1.2,
@@ -47,10 +48,24 @@ var tokyo = {
       this.hourlySalesArray.push(Math.floor(this.randomNumberOfCustomers() * this.avgCookieSalesPerCustomer));
     }
   },
+  renderSalesArrayAsUnorderedList: function(){
+    var mainElement = document.getElementById('test');
+    var ulElement = document.createElement('ul');
+    ulElement.textContent = `${this.storename} Sales Data:`;
+    mainElement.appendChild(ulElement);
+    for (var i = 0; i < this.hourlySalesArray.length; i++){
+      var liElement = document.createElement('li');
+      liElement.setAttribute('id', `li${i}`);
+      liElement.textContent = `${this.hourlySalesArray[i]}`;
+      ulElement.appendChild(liElement);
+    }
+  },
 };
+tokyo.generateRandomSalesData();
+tokyo.renderSalesArrayAsUnorderedList();
 
 var dubai = {
-  storename: 'dubai',
+  storename: 'Dubai',
   minimumCustomersPerHour: 11,
   maximumCustomersPerHour: 38,
   avgCookieSalesPerCustomer: 3.7,
@@ -64,10 +79,24 @@ var dubai = {
       this.hourlySalesArray.push(Math.floor(this.randomNumberOfCustomers() * this.avgCookieSalesPerCustomer));
     }
   },
+  renderSalesArrayAsUnorderedList: function(){
+    var mainElement = document.getElementById('test');
+    var ulElement = document.createElement('ul');
+    ulElement.textContent = `${this.storename} Sales Data:`;
+    mainElement.appendChild(ulElement);
+    for (var i = 0; i < this.hourlySalesArray.length; i++){
+      var liElement = document.createElement('li');
+      liElement.setAttribute('id', `li${i}`);
+      liElement.textContent = `${this.hourlySalesArray[i]}`;
+      ulElement.appendChild(liElement);
+    }
+  },
 };
+dubai.generateRandomSalesData();
+dubai.renderSalesArrayAsUnorderedList();
 
 var paris = {
-  storename: 'paris',
+  storename: 'Paris',
   minimumCustomersPerHour: 20,
   maximumCustomersPerHour: 38,
   avgCookieSalesPerCustomer: 2.3,
@@ -81,10 +110,24 @@ var paris = {
       this.hourlySalesArray.push(Math.floor(this.randomNumberOfCustomers() * this.avgCookieSalesPerCustomer));
     }
   },
+  renderSalesArrayAsUnorderedList: function(){
+    var mainElement = document.getElementById('test');
+    var ulElement = document.createElement('ul');
+    ulElement.textContent = `${this.storename} Sales Data:`;
+    mainElement.appendChild(ulElement);
+    for (var i = 0; i < this.hourlySalesArray.length; i++){
+      var liElement = document.createElement('li');
+      liElement.setAttribute('id', `li${i}`);
+      liElement.textContent = `${this.hourlySalesArray[i]}`;
+      ulElement.appendChild(liElement);
+    }
+  },
 };
+paris.generateRandomSalesData();
+paris.renderSalesArrayAsUnorderedList();
 
 var lima = {
-  storename: 'lima',
+  storename: 'Lima',
   minimumCustomersPerHour: 2,
   maximumCustomersPerHour: 16,
   avgCookieSalesPerCustomer: 4.6,
@@ -98,6 +141,19 @@ var lima = {
       this.hourlySalesArray.push(Math.floor(this.randomNumberOfCustomers() * this.avgCookieSalesPerCustomer));
     }
   },
+  renderSalesArrayAsUnorderedList: function(){
+    var mainElement = document.getElementById('test');
+    var ulElement = document.createElement('ul');
+    ulElement.textContent = `${this.storename} Sales Data:`;
+    mainElement.appendChild(ulElement);
+    for (var i = 0; i < this.hourlySalesArray.length; i++){
+      var liElement = document.createElement('li');
+      liElement.setAttribute('id', `li${i}`);
+      liElement.textContent = `${this.hourlySalesArray[i]}`;
+      ulElement.appendChild(liElement);
+    }
+  },
 };
-
+lima.generateRandomSalesData();
+lima.renderSalesArrayAsUnorderedList();
 
