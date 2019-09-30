bac2844976330b4a75025a4da675efb89cc96ed6
Conor <conormccue@gmail.com>
2019-09-30 13:45:52 -0700

    random number generator for each shop location

--- js/sales.js
+++ js/sales.js
@@ -4,24 +4,40 @@ var seattle = {
   minimumCustomersPerHour: 23,
   maximumCustomersPerHour: 65,
   avgCookieSalesPerCustomer: 6.3,
+  randomNumberOfCustomers: function(){
+    return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
+  },
 };
+
 var tokyo = {
   minimumCustomersPerHour: 3,
   maximumCustomersPerHour: 24,
   avgCookieSalesPerCustomer: 1.2,
+  randomNumberOfCustomers: function(){
+    return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
+  },
 };
 var dubai = {
   minimumCustomersPerHour: 11,
   maximumCustomersPerHour: 38,
   avgCookieSalesPerCustomer: 3.7,
+  randomNumberOfCustomers: function(){
+    return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
+  },
 };
 var paris = {
   minimumCustomersPerHour: 20,
   maximumCustomersPerHour: 38,
   avgCookieSalesPerCustomer: 2.3,
+  randomNumberOfCustomers: function(){
+    return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
+  },
 };
 var lima = {
   minimumCustomersPerHour: 2,
   maximumCustomersPerHour: 16,
   avgCookieSalesPerCustomer: 4.6,
+  randomNumberOfCustomers: function(){
+    return Math.floor((Math.random()) * (this.maximumCustomersPerHour - this.minimumCustomersPerHour) + this.minimumCustomersPerHour);
+  },
 };
