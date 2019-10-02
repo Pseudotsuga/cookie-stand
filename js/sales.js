1e616a6db8aff3353ae5d8c3c99851fbf6a44fa7
Conor <conormccue@gmail.com>
2019-10-01 20:22:23 -0700

    stopped overwriting total total

--- js/sales.js
+++ js/sales.js
@@ -1,6 +1,7 @@
 'use strict';
 var hoursOpenTitle = ['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM'];
 var initialNumberOfStores = 0;
+var initialTotalTotal = 0;
 
 function renderTableHead(){
   var mainElement = document.getElementById('target');
@@ -62,10 +63,8 @@ function renderTableFooter(){
   }
   tableDataElement = document.createElement('td');
   for(var k = 0; k < initialNumberOfStores; k++){
-    var initialTotalTotal = 0;
     var totalValues = document.getElementsByClassName('total');
     initialTotalTotal += parseInt(`${totalValues[k].textContent}`, 10);
-    console.log(initialTotalTotal);
   }
   tableDataElement.textContent = `${initialTotalTotal}`;
   tableRowElement.appendChild(tableDataElement);
@@ -91,7 +90,6 @@ StoreLocation.prototype.randomSalesData = function(){
   }
 };
 
-//consider establishing function parameters than can specify parent and child nodes to increase legibility.
 StoreLocation.prototype.renderSalesDataAsList = function(){
   var mainElement = document.getElementById('target');
   var ulElement = document.createElement('ul');
